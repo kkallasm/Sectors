871476764c0466c507473e14e65b6be0de9614a3
kkallasm <kaspar68@gmail.com>
2015-10-01 20:27:33 +0300

    config

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'root');
-	define('DB_HOST', 'localhost');
-    define('DB_NAME', 'helmes');
+	define('DB_USER', '<db_user>');
+	define('DB_PASSWORD', '<db_psw>');
+	define('DB_HOST', '<db_host>');
+    define('DB_NAME', '<db_name>');
     define('DB_PORT', 3306);
     define('BASE_URL','localhost/');
     
